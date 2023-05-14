88f7a5b85509b76d3b4e170db64ac571fe623fc3
AnSuHan <glorygem195@gmail.com>
2023-05-14 21:23:29 +0900

    for merge

--- project/lib/MailWrite.dart
+++ project/lib/MailWrite.dart
@@ -34,6 +34,7 @@ class MailWrite extends State<MailWriteStateful> {
               //MailScene().addMail(newobj);        //not adding
               MailScene.addMailStatic(newobj);    //range error
               //MailScene.addMailWithNewData(newobj);
+              //main에 merge하기 위해 commit한 주석
 
               setState(() {
                 //MailScene.mails.add(newobj);      //range error
